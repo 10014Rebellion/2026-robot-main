ffb31572f3f4a1fd97199240ea4b9bad308180d0
Aryan Kammili <aryan.kammili@gmail.com>
2026-02-16 16:51:44 -0600

    flywheel bang bang code screw you taha make this pretty latre

--- src/main/java/frc/robot/systems/shooter/Shooter.java
+++ src/main/java/frc/robot/systems/shooter/Shooter.java
@@ -1,7 +1,10 @@
 package frc.robot.systems.shooter;
 
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.filter.Debouncer.DebounceType;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.systems.shooter.flywheels.FlywheelsSS;
 import frc.robot.systems.shooter.fuelpump.FuelPumpSS;
@@ -12,12 +15,36 @@ public class Shooter {
   private final HoodSS mHoodSS;
   private final FlywheelsSS mFlywheelSS;
 
+  private Debouncer mFlywheelDebouncer = new Debouncer(0.25, DebounceType.kRising);
+
   public Shooter(FuelPumpSS pFuelPumpSS, HoodSS pHoodSS, FlywheelsSS pFlywheelSS) {
     this.mFuelPumpsSS = pFuelPumpSS; this.mHoodSS = pHoodSS; this.mFlywheelSS = pFlywheelSS; // I miss my C++ initializer lists :'(
   }
 
   public Command setFlywheelsRPSCmd(double pRPS) {
-    return new InstantCommand(() -> mFlywheelSS.setFlywheelSpeeds(pRPS));
+    return new InstantCommand(() -> mFlywheelSS.setFlywheelTorqueCurrent(pRPS), mFlywheelSS);
+  }
+
+  public Command setFlywheelsRPSBangBangCommand(double pRPS) {
+    return new FunctionalCommand(
+      () -> {}, 
+      () -> {
+        boolean inTolerance = Math.abs(mFlywheelSS.getFlywheelRPS() - pRPS) <= 5;
+        boolean atGoal = mFlywheelDebouncer.calculate(inTolerance);
+
+
+        if(atGoal){
+          mFlywheelSS.setFlywheelTorqueCurrent(40);
+        }
+
+        else{
+          mFlywheelSS.setFlywheelTorqueCurrent(100);
+        }
+      }, 
+
+      (interrupted) -> {mFlywheelSS.setFlywheelTorqueCurrent(30);}, 
+      () -> false ,
+      mFlywheelSS);
   }
 
   public Command setFlywheelsVoltsCmd(double pVolts) {
